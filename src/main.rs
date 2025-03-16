1d57f7365aa803dc9364affe201dc5e3a7d2e051
dijith-481 <dijithdinesh@protonmail.com>
2025-03-17 00:14:05 +0530

    seperated input

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,54 @@
 use std::{
-    io::{self, Read, stdin},
+    io::{self, Read, Write, stdin},
     os::fd::AsRawFd,
+    process::exit,
 };
 use termios::*;
 
-fn disable_raw_mode(original_termios: &Termios) -> io::Result<()> {
-    tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &original_termios)
+const fn CTRL_KEY(c: u8) -> u8 {
+    c & 0x1f
 }
-fn enable_raw_mode() -> io::Result<()> {
+
+fn disable_raw_mode(original_termios: &Termios) {
+    tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &original_termios).expect("tcsetattr")
+}
+fn enable_raw_mode() {
     let fd = io::stdin().as_raw_fd();
-    let mut termios = Termios::from_fd(fd)?;
-    termios.c_lflag &= !(ECHO | ICANON);
-    tcsetattr(fd, TCSAFLUSH, &termios)?;
-    Ok(())
+    let mut termios = Termios::from_fd(fd).expect("termios");
+    termios.c_iflag &= !(INPCK | ISTRIP | BRKINT | IXON | ICRNL);
+    termios.c_oflag &= !(OPOST);
+    termios.c_cflag |= CS8;
+    termios.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+    termios.c_cc[VMIN] = 0;
+    termios.c_cc[VTIME] = 1;
+
+    tcsetattr(fd, TCSAFLUSH, &termios).expect("tcsetattr");
+}
+fn editor_key_read() -> u8 {
+    let mut buffer = [0; 1];
+    while io::stdin().read(&mut buffer).expect("read") == 1 {}
+    buffer[0]
+}
+fn editor_process_keypress() -> u8 {
+    let c = editor_key_read();
+    match c {
+        c if c == CTRL_KEY(b'q') => b'0',
+        _ => c,
+    }
+}
+fn editor_refresh_screen() {
+    write!(io::stdout(), "\x1b[2J").expect("write");
 }
 fn main() -> io::Result<()> {
     let fd = io::stdin().as_raw_fd();
     let mut orig_termios = Termios::from_fd(fd)?;
-    tcgetattr(fd, &mut orig_termios)?;
-    enable_raw_mode()?;
-    let mut buffer = [0; 1];
-    while io::stdin().read(&mut buffer)? == 1 && buffer[0] != b'q' {
-        let c = buffer[0];
-        if c.is_ascii_control() {
-            println!("{}", c);
-        } else {
-            println!("{}{}", c, c as char)
+    tcgetattr(fd, &mut orig_termios).expect("tcgetattr");
+    enable_raw_mode();
+    loop {
+        // editor_refresh_screen();
+        let exitcode = editor_process_keypress();
+        if exitcode == b'0' {
+            break;
         }
     }
     disable_raw_mode(&orig_termios);
